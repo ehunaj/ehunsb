b4477fdfae70ded3fdbe20ea0797fa3e03d9072f
ehunaj <56645834+ehunaj@users.noreply.github.com>
2019-12-24 13:49:25 +0800

    Add files via upload

---
+++ b.py
@@ -0,0 +1,21 @@
+#===============EHUN  BOTS===============
+from salinan import LineBot
+import threading
+def login(resp, auth):
+    bot = LineBot(resp,auth)
+
+Ehn = threading.Thread(target=login, args=('Lia1',"EMlfK7mYwblyfFCbIqzc.xmOZkKoDi9P/nYUM4NOsla.No6WXHfb0MT5gsrTCcTeH1+6Ero8zgjbp3y1/6ReM3E=")).start()
+Ehn = threading.Thread(target=login, args=('Lia2',"EMksRf29SvvNfdrWFO7d.PVmbdG5nG77Km5hrSxbgtq.bo3/FFKijFqqYT3u0ocCHzdcI5pqF86TdgrNAjRUkXo=")).start()
+Ehn = threading.Thread(target=login, args=('Lia3',"EMsbHRI13gMR9de90UIe.NzWU4uHxu25IbVF0crqixG.Dc5LMyQswmK3KPFtNsfItElyDwuPl1sz/Oe1UxG4ASE=")).start()
+Ehn = threading.Thread(target=login, args=('Lia4',"EMkW57vys8n5pB9hqyY4.nPdFxcNp9N+djrQIkTL6za.F6EErHhtQFX2yt2clnLs3JBnTOdPwkGZx9qnsX+yrME=")).start()
+Ehn = threading.Thread(target=login, args=('Lia5',"EM9x7UxR28zXpRZlHWs6./P1jbnnzbpY+YBIdcOFj1G.UsK9kmCwqL7Ec8boTfhsEGUIPXhqXTXnYYK7OFLeqGA=")).start()
+#Ehn = threading.Thread(target=login, args=('Lia6',"EMn7qN83MRA63x9cdod0.obukIy+rSD5uCVIy6Toaua./d+z/a0E2wS3Wfr+C1h9kisbz5DGYV64LI4XN4JDpDY=")).start()
+#Ehn = threading.Thread(target=login, args=('Lia7',"ELWYDGUbsLa
+#Ehn = threading.Thread(target=login, args=('Lia8',"ELWYDGUbsLa
+#Ehn = threading.Thread(target=login, args=('Lia9',"ELWYDGUbsLa
+#Ehn = threading.Thread(target=login, args=('Lia10',"ELWYDGUbsLa
+#Ehn = threading.Thread(target=login, args=('Lia11'"ELWYDGUbsLa
+#Ehn = threading.Thread(target=login, args=('Lia12',"ELWYDGUbsLa
+#print("SUCCES")  
+
+
